68bcc6e7b1d01d015c0d83c20a2f6f6517286556
sandesh-chaudhary23 <sandeshskta@gmail.com>
2026-05-24 00:33:58 +0000

    conversion of datatyper

---
+++ basics-01/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33"
+//console.log(typeof score);
+//console.log(typeof[score]);
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+// "33"=>33;
+//"33abc"=>NaN
+//true=>1; false=>0
+let isLoggedIn="frankin"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+//1=>true; 0=>false
+//""=>false
+// "frankin"=>true
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
