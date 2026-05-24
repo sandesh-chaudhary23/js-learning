cf1d0a527302d2c5b0a444fa93ee778744357f47
sandesh-chaudhary23 <sandeshskta@gmail.com>
2026-05-24 00:36:02 +0000

    conversion of data  types

--- basics-01/03_conversionOperation.js
+++ basics-01/03_conversionOperation.js
@@ -16,4 +16,4 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber = 44
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);  
