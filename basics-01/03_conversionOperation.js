ac40ea495c770181864d059213603ca5be8a6d48
sandesh-chaudhary23 <sandeshskta@gmail.com>
2026-05-24 01:45:41 +0000

    conversion practise

--- basics-01/03_conversionOperation.js
+++ basics-01/03_conversionOperation.js
@@ -15,5 +15,36 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 // "frankin"=>true
 let someNumber = 44
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);  
+// console.log(stringNumber);
+// console.log(typeof stringNumber);  
+
+
+//operations...
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+let str1="hello"
+let str2=" frankin"
+let str3 =str1 +str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2"); 
+//bad practise insted use brackets 
+// console.log((3+4)*5%3);
+
+//  console.log(+true);
+//  console.log(+"");
+// num1=num2=num3=2+2 (bad practise )
+// let gameCounter=100
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
