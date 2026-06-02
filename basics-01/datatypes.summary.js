12e9a2dd30a8e00dbe348dac780d3007aefda9bb
sandesh-chaudhary23 <sandeshskta@gmail.com>
2026-06-02 01:31:34 +0000

    datatypes summary

---
+++ basics-01/datatypes.summary.js
@@ -0,0 +1,31 @@
+// primitive datatypes 
+
+// 7 types :string,number,boolean,null,undefined,symbol,BigInt
+const score =100
+const scoreValue=100.33
+const isloggedIn = false
+const outsideTemp =null
+let userEmail;
+const id  =symbol("123")
+const anotherid  =symbol("123")
+const bigNumber =33387687938n
+
+// refernce (non primitive)
+// array, objects ,functions
+const avengers =["Captain America","ironMan","Thor","Hulk"];
+let myobject ={
+    name:"frankin",
+    age:23,
+}
+const myFunction = function(){
+    console.log("Avengers Assembelled")
+}
+// all return type is function 
+//  function value = function object 
+// to know the type of vlaue 
+console.log(typeof bigNumber);
+null =object 
+undefiend = undefiend 
+boolean =boolean 
+string =string 
+number =number  
