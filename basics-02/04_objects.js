676f870bbaa8458b8c799f87da4ab4d26f2f6892
sandesh-chaudhary23 <sandeshskta@gmail.com>
2026-06-12 13:18:17 +0000

    object destructure

--- basics-02/04_objects.js
+++ basics-02/04_objects.js
@@ -4,22 +4,72 @@
 // tinderUnser.name="franikin"
 // tinderUnser.isLoggedIn= false 
  
+// // console.log(tinderUnser)
+
+// const regularuser={
+//     email:"frankin@blabla",
+//     fullname:{
+//         userfullname:{
+//             firstname:"frankin",
+//             lastname:"uuuuuuu"
+//         }
+//     }
+// } 
+// console.log(regularuser.fullname.userfullname.firstname); // can use ? in the objects as something might not present so it will not show the error .
+
+// const obj1 ={1:"a",2:"b"}
+// const obj2 ={3:"a",4:"b"}
+
+// // const obj3 ={obj1,obj2} // it will make object inside object 
+// // const obj3c= Object.assign({},obj1,obj2) //{} an optional parameter ggod for reuslt 
+// const obj3 = {...obj1,...obj2} // best way and simple 
+
+// console.log(obj3);
+
+// const users =[ // dataa comes in array which stires objects in it 
+//     {
+//         id : 1,
+//         email:"frankin@gmail.com"
+//     }
+//     {
+//         id : 1,
+//         email:"frankin@gmail.com"
+//     } 
+//     {
+    
+//         id : 1,
+//         email:"frankin@gmail.com"
+//     }  
+// ]
+// console.log(users[1].email);
 // console.log(tinderUnser)
 
-const regularuser={
-    email:"frankin@blabla",
-    fullname:{
-        userfullname:{
-            firstname:"frankin"
-            lastname:"uuuuuuu"
-        }
-    }
-} 
-console.log(regularuser.fullname.userfullname.firstname); // can use ? in the objects as something might not present so it will not show the error .
+// console.log(Object.keys(tinderUnser));
+// console.log(Object.values(tinderUnser));
+// console.log(Object.entries(tinderUnser));
 
-const obj1 ={1:"a",2:"b"}
-const obj2 ={3:"a",4:"b"}
+// console.log(tinderUnser.hasOwnProperty('isLogged '));
 
-// const obj3 ={obj1,obj2} // it will make object inside object 
-cosnt obj3c= Object.assign({},obj1,obj2) //{} an optional parameter ggod for reuslt 
-console.log(obj3);
+const course ={
+    coursename :"js in hindi",
+    price: "999",
+    courseInstructor:"frankin"
+
+}
+// course.courseInstructor
+
+const {courseInstructor: Instructor}= course
+console.log(Instructor);
+
+//api
+
+// {
+//     "name":"frankin",
+//     "coursename" :"js in hindi",
+//     "price": "999",
+// }
+[
+    {},
+    {},
+    {}
+]
